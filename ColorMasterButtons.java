d01ab3a12e508afc4711fff8c8d155f371a05c2c
Lancelot <903163212@qq.com>
2022-11-15 21:27:01 +0800

    支持图形和文字，利用Java自带的调色盘实现颜色切换

--- ColorMasterButtons.java
+++ ColorMasterButtons.java
@@ -1,21 +1,16 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+
 
 public class ColorMasterButtons extends JPanel {
     public ColorMasterButtons(Board DrawingBoard) {
         JButton SelectColorButton = new JButton("ChangeColor");
         SelectColorButton.addActionListener(
-                new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        JColorChooser colorChooser = new JColorChooser();
-                        Color color = colorChooser.showDialog(SelectColorButton, "Select color", Color.lightGray);
-                        if (color == null)
-                            color = Color.BLACK;
-                        DrawingBoard.SetColor(color);
-                    }
+                e -> {
+                    Color color = JColorChooser.showDialog(SelectColorButton, "Select color", Color.lightGray);
+                    if (color == null)
+                        color = Color.BLACK;
+                    DrawingBoard.SetColor(color);
                 });
         this.setLayout(new FlowLayout());
         this.add(SelectColorButton);
